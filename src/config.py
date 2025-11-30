6ca45dd135a10932c33b7f6b657e7862c8cfb28a
MartinFuchs1 <fuchsmmartin@gmail.com>
2025-11-30 17:46:20 +0100

    add Config module

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        if not self.api_key:
+            raise RuntimeError("OPENAI_API_KEY is not set in the enviroment.")
+        self.model = "gpt-4o-mini"
